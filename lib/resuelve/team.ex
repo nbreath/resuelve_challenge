1a008509071c1815452c718d8e19d4dd1cc7b8fc
John <nismbreath@gmail.com>
2020-08-14 18:21:44 -0500

    Improve error managment and validation

--- lib/resuelve/team.ex
+++ lib/resuelve/team.ex
@@ -1,5 +1,5 @@
 defmodule Resuelve.Team do
-  @level_score %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
+  @team_level_scores %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
 
   def calculate_score_percentage(individual_score, level_score)
       when individual_score > level_score,
@@ -13,7 +13,7 @@ defmodule Resuelve.Team do
     {team_score, team_level_score} =
       Enum.reduce(players, {0, 0}, fn %{"goles" => score, "nivel" => level},
                                       {player_score_acc, level_score_acc} ->
-        {player_score_acc + score, level_score_acc + @level_score[level]}
+        {player_score_acc + score, level_score_acc + @team_level_scores[level]}
       end)
 
     calculate_score_percentage(team_score, team_level_score)
@@ -28,29 +28,33 @@ defmodule Resuelve.Team do
                            "sueldo" => salary,
                            "bono" => bonus
                          } = player ->
-      individual_score = calculate_score_percentage(score, @level_score[level])
+      individual_score = calculate_score_percentage(score, @team_level_scores[level])
       total_player_percentage = (individual_score + team_score_percentage) / 2
       full_salary = salary + total_player_percentage * bonus
 
       player
       |> Map.delete("nivel")
       |> Map.merge(%{
-        "goles_minimos" => @level_score[level],
+        "goles_minimos" => @team_level_scores[level],
         "sueldo_completo" => round(full_salary)
       })
     end)
   end
 
   def process_team_salaries(players_binary) do
-    case Poison.decode(players_binary) do
-      {:ok, parsed_data} ->
-        players_salaries =
-          parsed_data
-          |> calculate_team_full_salaries()
+    with {:ok, parsed_data} when is_list(parsed_data) and length(parsed_data) > 0 <-
+           Poison.decode(players_binary) do
+      players_salaries = calculate_team_full_salaries(parsed_data)
 
-        {:ok, players_salaries}
+      {:ok, players_salaries}
+    else
+      {:ok, _} ->
+        {:error, "Not supported structure"}
 
       {:error, error} ->
+        {:error, error}
+
+      {:error, error, _} ->
         {:error, error}
     end
   end
